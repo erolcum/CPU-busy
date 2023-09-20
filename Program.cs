43f577d5b8cfea512a119a5676f34de90b984999
Erol Iscioglu <110387801+erolcum@users.noreply.github.com>
2023-09-20 16:21:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+class ThreadDemo
+{
+    static private void busyLoop()
+    {
+        long count;
+        for (count = 0; count < 1000000000000L; count = count + 1)
+        {
+        }
+    }
+
+    static void Main()
+    {
+        for (int i = 0; i < 100; i = i + 1)
+        {
+            Thread t1 = new Thread(busyLoop);
+            t1.Start();
+        }
+    }
+}
